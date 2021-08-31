29c1f306c5d58f6903f714b6641100c96923c058
Rick King <86253101+rkinginflux@users.noreply.github.com>
2021-08-31 13:49:38 -0600

    Update urldecode.go

--- urldecode/urldecode.go
+++ urldecode/urldecode.go
@@ -1,4 +1,4 @@
-// Description: Decode URL's from Papertrail logs
+// Description: Decode URL's
 // e.g. echo "GET /query?db=statsd&epoch=s&q=SELECT+last%28%22value%22%29+FROM+%221minute%22.%22custom_fax_sent_faxSentOrderFailures%22+WHERE+time+%3E+now%28%29+-+15m+GROUP+BY+time%281m%29%2C+%22type%22+fill%28null%29 HTTP/1.1" | urldecode
 
 package main
