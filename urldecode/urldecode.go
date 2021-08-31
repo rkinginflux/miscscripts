121f451050a088a963ac792154da7c10360ecb15
Rick King <86253101+rkinginflux@users.noreply.github.com>
2021-08-31 13:30:19 -0600

    Update urldecode.go

--- urldecode/urldecode.go
+++ urldecode/urldecode.go
@@ -1,1 +1,25 @@
+// Description: Decode URL's from Papertrail logs
+// e.g. echo "GET /query?db=statsd&epoch=s&q=SELECT+last%28%22value%22%29+FROM+%221minute%22.%22custom_fax_sent_faxSentOrderFailures%22+WHERE+time+%3E+now%28%29+-+15m+GROUP+BY+time%281m%29%2C+%22type%22+fill%28null%29 HTTP/1.1" | urldecode
 
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"log"
+	"net/url"
+)
+
+func main() {
+	fmt.Println("data is from pipe")
+	bytes, _ := ioutil.ReadAll(os.Stdin)
+	str := string(bytes)
+	fmt.Print(str)
+	path := (str)
+	unescapedPath, err := url.PathUnescape(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(unescapedPath)
+}
